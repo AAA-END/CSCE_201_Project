4f8b268a87bb630d8d83d36b988834d24a59f80b
ded42 <ded42@DESKTOP-2N0UFP5>
2025-03-30 15:53:08 -0800

    Add project files.

---
+++ CSCE_201_Project.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+int intCheck(char check_num[]);
+
+void showMenu();
+
+int getAnswer();
+
+int setSpeed();
+
+void playGame();
+
+
+
+
+int main()
+{
+	int menu_mode = 0;
+	int game_speed = 20;
+
+	while (menu_mode != 3) {
+		showMenu();
+		menu_mode = getAnswer();
+
+		if (menu_mode == 1) {
+			playGame();
+		}
+		else if (menu_mode == 2) {
+			game_speed = setSpeed();
+		}
+	}
+}
+
+
+
+
+
+int intCheck(char check_num[])
+{
+	bool check_again = false;
+	for (int i = 0; i < 9; i++) {
+		if (check_again == true)
+			break;
+		if (check_num[i] == NULL)
+			break;
+		if (!(isdigit(check_num[i]))) {
+			check_again = true;
+		}
+	}
+
+	if (check_again == true) {
+		cout << "Error: Please enter a valid number\n";
+		cout << "Please try again: ";
+		cin >> check_num;
+		intCheck(check_num);
+	}
+	else {
+		return atoi(check_num);
+	}
+}
+
+void showMenu() 
+{
+	cout << "1. Play game\n";
+	cout << "2. Adjust settings\n";
+	cout << "3. Exit game\n";
+}
+
+int getAnswer()
+{
+	int menu_answer = 0;
+	char input_answer[9];
+	while ((menu_answer > 3) && (menu_answer < 1)) {
+		cout << "\nEnter an input number: ";
+		cin >> input_answer;
+		menu_answer = intCheck(input_answer);
+		if ((menu_answer > 3) && (menu_answer < 1))
+			cout << "Error: input number out of range";
+	}
+	return menu_answer;
+}
+
+void playGame() {
+	cout << "game working" << endl;
+}
+
+int setSpeed() {
+	cout << "menu working" << endl;
+}
